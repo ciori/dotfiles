b4bae74e21b37711f93f54f4bb7848381672b288
ciori <ciori@posteo.net>
2025-04-17 11:29:33 +0200

    Update dotfiles

--- alpine-labwc-desktop/scripts/labwc.sh
+++ alpine-labwc-desktop/scripts/labwc.sh
@@ -2,3 +2,14 @@
 
 # install labwc
 doas apk add labwc labwc-doc xwayland foot wofi swaylock swaybg swayidle
+
+# install applications
+doas apk add librewolf mako waybar pcmanfm vim
+
+# create home directories
+mkdir ~/pictures
+mkdir ~/downloads
+
+# copy dotfiles
+cp -R ../configs/.profile ~/.profile
+cp -R ../configs/.configs ~/
